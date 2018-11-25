04c94941cd5b34dccf8ad859c7f5715f2b45427c
LakersFan24 <kiran.saravana@gmail.com>
2018-11-25 17:37:09 -0500

    Add files via upload

--- app/src/main/java/com/gmu/kam/cs321partapp/carWebsites.js
+++ app/src/main/java/com/gmu/kam/cs321partapp/carWebsites.js
@@ -3,60 +3,44 @@ var globalArr = "global"
 function walmartSearch(make, model, year, item)
 {
   var w = window.open("https://www.walmart.com");
-  //var document = new Document();
-  //alert(document.url);
-  //document.url = "https://www.walmart.com";
-  //alert(document.url);
+
   var s = make+" "+model+" "+year+" "+item;
-  //alert(document.url);
+
   w.onload = function(){
 
-  var id = w.document.getElementById('Search');
-  id.value = s;
-  alert("Hello World");
-};
+    var id = w.document.getElementById('Search');
+    id.value = s;
+    alert("Hello World");
+  };
 
-  //alert("*******");
-  //w.document.forms[0].submit();
 }
 
 function usaSearch(make, model, year, item)
 {
   var w = window.open("https://www.usautoparts.net");
   var w = open
-  //var document = new Document();
-  //alert(document.url);
-  //document.url = "https://www.walmart.com";
-  //alert(document.url);
-  //alert(w.onload);
+
   var s = make+" "+model+" "+year+" "+item;
-  //alert(document.url);
-  // w.document.addEventListener("DOMContentLoaded",
+
   w.onload = function(){
 
-  var id = w.document.getElementById('Search');
-  id.value = s;
-  alert("Hello World");
-};
-  //alert("*******");
-  //w.document.forms[0].submit();
+    var id = w.document.getElementById('Search');
+    id.value = s;
+    alert("Hello World");
+  };
+
 }
 
 function carPartsSearch(make, model, year, item)
 {
   window.open("https://www.carparts.com");
   var s = make+" "+model+" "+year+" "+item;
-  // document.getElementById('global-search-input').value=s;
-  // document.forms[0].submit();
-
-  //alert("*******");
-  //w.document.forms[0].submit();
   w.onload = function(){
 
-  var id = w.document.getElementById('Search');
-  id.value = s;
-  alert("Hello World");
-};
+    var id = w.document.getElementById('Search');
+    id.value = s;
+    alert("Hello World");
+  };
 }
 
 function carIdSearch(make, model, year, item)
@@ -64,12 +48,10 @@ function carIdSearch(make, model, year, item)
   var s = make+" "+model+" "+year+" "+item;
   w.onload = function(){
 
-  var id = w.document.getElementById('Search');
-  id.value = s;
-  alert("Hello World");
-};
-  //alert("*******");
-  //w.document.forms[0].submit();
+    var id = w.document.getElementById('Search');
+    id.value = s;
+    alert("Hello World");
+  };
 }
 
 //walmartSearch("toyota", "corolla", "2016", "tires");
