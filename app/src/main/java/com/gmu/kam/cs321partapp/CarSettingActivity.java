8113cc88f3d35afc0c4d0cf0b60bd72f335fb54a
Jon <jcbarker33@gmail.com>
2018-11-24 21:11:01 -0500

    Changed the layout

--- app/src/main/java/com/gmu/kam/cs321partapp/CarSettingActivity.java
+++ app/src/main/java/com/gmu/kam/cs321partapp/CarSettingActivity.java
@@ -2,42 +2,27 @@ package com.gmu.kam.cs321partapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.util.ArrayList;
 
 public abstract class CarSettingActivity extends AppCompatActivity {
-    //ArrayList for holding the car objects
-    ArrayList CarList = new ArrayList(4);
+    static String Year = null;
+    static String make = null;
+    static String model = null;
+    
+
     //method for adding cars
-    public void newCar(){
-
-        //need to input the information here
-        String year = "";
-        String make = "";
-        String model = "";
-        //
-        Car car = new Car();
-        car.setYear(year);
-        car.setMake(make);
-        car.setModel(model);
-        
-        //adding the new car
-        CarList.add(car);
+    public void AddCar(){
     }
 
-    public boolean removeCar(Car car){
-        CarList.remove(car);
-        return false;
-    }
-
-    public void EditCar(){
+    public void RemoveCar(Car car){
 
     }
+    //
 
     public void SaveData(){
 
     }
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
